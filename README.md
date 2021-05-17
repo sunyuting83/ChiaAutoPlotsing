2ab18a51e2a8d6c3d5c7a8b66e57687193867ca3
sun <sleepsun123@163.com>
2021-05-17 22:07:26 +0800

    add command for linux

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Chia Auto Plotsing
+
+## Donate
+XCH:
+``` 
+xch1xfq4nlgs7cu3lp0cwe96ucsrr6y24nyjzapjy0q5yuj9da6z2g0s6fadzc
+```
--- app.go
+++ app.go
@@ -14,7 +14,6 @@ import (
 	"sync"
 	"time"
 
-	"golang.org/x/text/encoding/simplifiedchinese"
 	"gopkg.in/yaml.v2"
 )
 
@@ -23,15 +22,16 @@ var (
 )
 
 type Config struct {
-	NumPlots  string `yaml:"NumPlots"`
-	KSize     string `yaml:"KSize"`
-	Buffer    string `yaml:"Buffer"`
-	Threads   string `yaml:"Threads"`
-	Buckets   string `yaml:"Buckets"`
-	TempPath  string `yaml:"TempPath"`
-	FinalPath string `yaml:"FinalPath"`
-	Total     int    `yaml:"Total"`
-	Sleep     int    `yaml:"Sleep"`
+	NumPlots  string   `yaml:"NumPlots"`
+	KSize     string   `yaml:"KSize"`
+	Buffer    string   `yaml:"Buffer"`
+	Threads   string   `yaml:"Threads"`
+	Buckets   string   `yaml:"Buckets"`
+	TempPath  string   `yaml:"TempPath"`
+	FinalPath []string `yaml:"FinalPath"`
+	Total     int      `yaml:"Total"`
+	Sleep     int      `yaml:"Sleep"`
+	RunPath   string   `yaml:"RunPath"`
 }
 
 func main() {
@@ -54,56 +54,76 @@ func main() {
 		fmt.Println("读取配置文件出错")
 		os.Exit(0)
 	}
-
-	homedir, err := GetUserInfo()
-	if err != nil {
-		fmt.Println("获取用户目录失败")
-		os.Exit(0)
+	var (
+		ChiaAppPath string = confYaml.RunPath
+		rootPath    string = confYaml.RunPath
+		appName     string = "chia"
+		LogPath     string
+	)
+	if runtime.GOOS == "windows" {
+		homedir, err := GetUserInfo()
+		if err != nil {
+			fmt.Println("获取用户目录失败")
+			os.Exit(0)
+		}
+		rootPath = strings.Join([]string{homedir, `AppData\Local\chia-blockchain`}, `\`)
+		appName = "chia.exe"
 	}
-	var ChiaAppPath string
-	rootPath := strings.Join([]string{homedir, `AppData\Local\chia-blockchain`}, `\`)
 	if !IsDir(rootPath) {
 		fmt.Println("获取Chia运行目录失败")
 		os.Exit(0)
 	}
-	if !IsDir(confYaml.TempPath) {
+	if len(confYaml.FinalPath) > 0 {
+		for _, item := range confYaml.FinalPath {
+			if !IsDir(item) {
+				fmt.Println(strings.Join([]string{"获取缓存目录", item, "失败，请检查配置文件"}, " "))
+				os.Exit(0)
+			}
+		}
+	} else {
 		fmt.Println("获取缓存目录失败，请检查配置文件")
 		os.Exit(0)
 	}
-	if !IsDir(confYaml.FinalPath) {
+	LogPath = strings.Join([]string{CurrentPath, "log"}, "/")
+	if !IsDir(LogPath) {
+		err := os.Mkdir(LogPath, os.ModePerm)
+		if err != nil {
+			fmt.Println("创建日志目录失败，请重试")
+			os.Exit(0)
+		}
+	}
+	if !IsDir(confYaml.TempPath) {
 		fmt.Println("获取数据目录失败，请检查配置文件")
 		os.Exit(0)
 	}
-	files, _ := ioutil.ReadDir(rootPath)
-	var versionNumber []string
-	for _, f := range files {
-		if strings.Contains(f.Name(), "app-") {
-			versionNumber = append(versionNumber, string(f.Name()))
+	if runtime.GOOS == "windows" {
+		files, _ := ioutil.ReadDir(rootPath)
+		var versionNumber []string
+		for _, f := range files {
+			if strings.Contains(f.Name(), "app-") {
+				versionNumber = append(versionNumber, string(f.Name()))
+			}
+		}
+		ChiaAppPath = strings.Join([]string{rootPath, versionNumber[0], `resources\app.asar.unpacked\daemon`}, `\`)
+		if len(versionNumber) > 1 {
+			n := len(versionNumber) - 1
+			ChiaAppPath = strings.Join([]string{rootPath, versionNumber[n], `resources\app.asar.unpacked\daemon`}, `\`)
 		}
 	}
-	ChiaAppPath = strings.Join([]string{rootPath, versionNumber[0], `resources\app.asar.unpacked\daemon`}, `\`)
-	if len(versionNumber) > 1 {
-		n := len(versionNumber) - 1
-		ChiaAppPath = strings.Join([]string{rootPath, versionNumber[n], `resources\app.asar.unpacked\daemon`}, `\`)
-	}
-	farmKey, poolKey := GetPublicKey(ChiaAppPath)
+	ChiaExec := GetChieExec(ChiaAppPath)
+	farmKey, poolKey := GetPublicKey(ChiaAppPath, ChiaExec)
 
-	ChiaExec := strings.Join([]string{ChiaAppPath, "chia.exe"}, `\`)
-	// params := []string{"/C", "start", ChiaExec, "plots", "create", "-k", "32", "-b", "4000", "-r", "2", "-f", farmKey, "-p", poolKey, "-t", "d:", "-d", "d:", ">>", Path}
-	ChiaCmd := strings.Join([]string{"start", ChiaExec, "plots", "create", "-n", confYaml.NumPlots, "-k", confYaml.KSize, "-b", confYaml.Buffer, "-r", confYaml.Threads, "-f", farmKey, "-p", poolKey, "-t", confYaml.TempPath, "-d", confYaml.FinalPath}, " ")
-	for i := 0; i < confYaml.Total; i++ {
-		go RunExec(ChiaCmd)
-		time.Sleep(time.Duration(confYaml.Sleep) * time.Second)
-	}
-	startTime := time.Now().Format("2006-01-02 15:04:05")
-	appName := "chia.exe"
+	ChiaCmd := strings.Join([]string{ChiaExec, "plots", "create", "-n", confYaml.NumPlots, "-k", confYaml.KSize, "-b", confYaml.Buffer, "-r", confYaml.Threads, "-f", farmKey, "-p", poolKey, "-t", confYaml.TempPath, "-d", confYaml.FinalPath[0]}, " ")
+	StartPlots(ChiaCmd, LogPath, *confYaml)
+
 	task := func() {
 		status, _, _, _ := isProcessExist(appName)
 		if !status {
 			// content := strings.Join([]string{mailYaml.Content, time.Now().Format("2006-01-02 15:04:05")}, " 完成于 ")
 			// title := strings.Join([]string{mailYaml.Title, startTime}, " 开始于 ")
 			wg.Add(1)
-			fmt.Println(startTime + "done")
+			time.Sleep(time.Duration(60) * time.Second)
+			fmt.Println("done")
 			wg.Wait()
 		}
 	}
@@ -118,10 +138,22 @@ func main() {
 	<-ch
 }
 
-func RunExec(ChiaExec string) {
-	cmd := exec.Command("cmd", "/C", ChiaExec)
-	fmt.Println("CmdAndChangeDirToFile", cmd.Args)
+func RunExec(ChiaExec, LogPath string) {
+	cmd := exec.Command("nohup", ChiaExec, ">", LogPath, "2>&1")
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/C", "start", ChiaExec)
+	}
+	fmt.Println(cmd.Args)
 	cmd.Start()
+}
+func StartPlots(ChiaCmd, LogPath string, confYaml Config) {
+	for i := 0; i < confYaml.Total; i++ {
+		startTime := time.Now().Format("20060102")
+		LogFileName := strings.Join([]string{startTime, "_", strconv.Itoa(i), ".log"}, "")
+		logPath := strings.Join([]string{LogPath, LogFileName}, "/")
+		go RunExec(ChiaCmd, logPath)
+		time.Sleep(time.Duration(confYaml.Sleep) * time.Second)
+	}
 }
 
 func IsDir(path string) bool {
@@ -138,8 +170,7 @@ func GetUserInfo() (homedir string, err error) {
 	}
 	return u.HomeDir, nil
 }
-func GetPublicKey(ChiaAppPath string) (farmKey, poolKey string) {
-	ChiaExec := strings.Join([]string{ChiaAppPath, "chia.exe"}, `\`)
+func GetPublicKey(ChiaAppPath, ChiaExec string) (farmKey, poolKey string) {
 	c := exec.Command(ChiaExec, "keys", "show")
 	pwdOutput, _ := c.Output()
 	pwdLine := strings.Split(string(pwdOutput), "\r\n")
@@ -156,18 +187,15 @@ func GetPublicKey(ChiaAppPath string) (farmKey, poolKey string) {
 	return
 }
 
-func ConvertByte2String(byte []byte, charset string) string {
-	var str string
-	switch charset {
-	case "GB18030":
-		var decodeBytes, _ = simplifiedchinese.GB18030.NewDecoder().Bytes(byte)
-		str = string(decodeBytes)
-	case "UTF8":
-		fallthrough
-	default:
-		str = string(byte)
+func GetChieExec(ChiaAppPath string) (ChiaExec string) {
+	ChiaExe := "chia"
+	LineString := `\`
+	if runtime.GOOS == "windows" {
+		ChiaExe = "chia.exe"
+		LineString = "/"
 	}
-	return str
+	ChiaExec = strings.Join([]string{ChiaAppPath, ChiaExe}, LineString)
+	return
 }
 
 func isProcessExist(appName string) (bool, string, int, int) {
