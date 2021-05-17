9028b244b41dce3b5a1929737b52b31f4eac2ddc
sun <sleepsun123@163.com>
2021-05-18 00:25:05 +0800

    write a bug

--- app.go
+++ app.go
@@ -13,6 +13,7 @@ import (
 	"strings"
 	"sync"
 	"time"
+	"unsafe"
 
 	"gopkg.in/yaml.v2"
 )
@@ -81,7 +82,7 @@ func main() {
 			}
 		}
 	} else {
-		fmt.Println("获取缓存目录失败，请检查配置文件")
+		fmt.Println("获取数据目录失败，请检查配置文件")
 		os.Exit(0)
 	}
 	LogPath = strings.Join([]string{CurrentPath, "log"}, "/")
@@ -93,7 +94,7 @@ func main() {
 		}
 	}
 	if !IsDir(confYaml.TempPath) {
-		fmt.Println("获取数据目录失败，请检查配置文件")
+		fmt.Println("获取缓存目录失败，请检查配置文件")
 		os.Exit(0)
 	}
 	if runtime.GOOS == "windows" {
@@ -113,18 +114,22 @@ func main() {
 	ChiaExec := GetChieExec(ChiaAppPath)
 	farmKey, poolKey := GetPublicKey(ChiaAppPath, ChiaExec)
 
-	ChiaCmd := strings.Join([]string{ChiaExec, "plots", "create", "-n", confYaml.NumPlots, "-k", confYaml.KSize, "-b", confYaml.Buffer, "-r", confYaml.Threads, "-f", farmKey, "-p", poolKey, "-t", confYaml.TempPath, "-d", confYaml.FinalPath[0]}, " ")
-	StartPlots(ChiaCmd, LogPath, *confYaml)
+	StartPlots(LogPath, CurrentPath, ChiaExec, farmKey, poolKey, *confYaml)
 
 	task := func() {
 		status, _, _, _ := isProcessExist(appName)
 		if !status {
 			// content := strings.Join([]string{mailYaml.Content, time.Now().Format("2006-01-02 15:04:05")}, " 完成于 ")
 			// title := strings.Join([]string{mailYaml.Title, startTime}, " 开始于 ")
-			wg.Add(1)
-			time.Sleep(time.Duration(60) * time.Second)
-			fmt.Println("done")
-			wg.Wait()
+			time.Sleep(time.Duration(180) * time.Second)
+			current := GetCurrentNumber(CurrentPath, len(confYaml.FinalPath)-1)
+			if current <= 0 {
+				wg.Add(1)
+				fmt.Println("done")
+				wg.Wait()
+				os.Exit(0)
+			}
+			StartPlots(LogPath, CurrentPath, ChiaExec, farmKey, poolKey, *confYaml)
 		}
 	}
 	var ch chan int
@@ -146,7 +151,10 @@ func RunExec(ChiaExec, LogPath string) {
 	fmt.Println(cmd.Args)
 	cmd.Start()
 }
-func StartPlots(ChiaCmd, LogPath string, confYaml Config) {
+func StartPlots(LogPath, CurrentPath, ChiaExec, farmKey, poolKey string, confYaml Config) {
+	current := GetCurrentNumber(CurrentPath, len(confYaml.FinalPath)-1)
+
+	ChiaCmd := strings.Join([]string{ChiaExec, "plots", "create", "-n", confYaml.NumPlots, "-k", confYaml.KSize, "-b", confYaml.Buffer, "-r", confYaml.Threads, "-f", farmKey, "-p", poolKey, "-t", confYaml.TempPath, "-d", confYaml.FinalPath[current]}, " ")
 	for i := 0; i < confYaml.Total; i++ {
 		startTime := time.Now().Format("20060102")
 		LogFileName := strings.Join([]string{startTime, "_", strconv.Itoa(i), ".log"}, "")
@@ -199,6 +207,7 @@ func GetChieExec(ChiaAppPath string) (ChiaExec string) {
 }
 
 func isProcessExist(appName string) (bool, string, int, int) {
+	// 做win的判断  这里没做完
 	appary := make(map[string]int)
 	cmd := exec.Command("cmd", "/C", "tasklist")
 	output, _ := cmd.Output()
@@ -245,7 +254,7 @@ func GetCurrentPath() (string, error) {
 	//fmt.Println("path222:", path)
 	i := strings.LastIndex(path, "/")
 	if i < 0 {
-		err := errors.New(`Can't find "/" or "\".`)
+		err := errors.New("can't find file")
 		return "", err
 	}
 	return string(path[0 : i+1]), nil
@@ -260,4 +269,48 @@ func PathExists(path string) (bool, error) {
 		return false, nil
 	}
 	return false, err
+}
+
+func IsExist(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil || os.IsExist(err)
+}
+
+func Int2Byte(data int) (ret []byte) {
+	// 数字转Byte
+	var len uintptr = unsafe.Sizeof(data)
+	ret = make([]byte, len)
+	var tmp int = 0xff
+	var index uint = 0
+	for index = 0; index < uint(len); index++ {
+		ret[index] = byte((tmp << (index * 8) & data) >> (index * 8))
+	}
+	return ret
+}
+
+func Byte2Int(data []byte) int {
+	// Byte转数字
+	var ret int = 0
+	var len int = len(data)
+	var i uint = 0
+	for i = 0; i < uint(len); i++ {
+		ret = ret | (int(data[i]) << (i * 8))
+	}
+	return ret
+}
+
+func GetCurrentNumber(CurrentPath string, current int) (n int) {
+	NumberData := strings.Join([]string{CurrentPath, "nb"}, "/")
+	if IsExist(NumberData) {
+		number, err := ioutil.ReadFile(NumberData)
+		if err != nil {
+			return 0
+		}
+		return Byte2Int(number)
+	} else {
+		os.Create(NumberData)
+		number := Int2Byte(current)
+		ioutil.WriteFile(NumberData, number, 0644)
+		return current
+	}
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module autoplots
 
 go 1.15
 
-require (
-	golang.org/x/text v0.3.6
-	gopkg.in/yaml.v2 v2.4.0
-)
+require gopkg.in/yaml.v2 v2.4.0
